440d8ea747b5162307067aaeca2bc816e5f54455
Anushka Dole <90973768+adole2008@users.noreply.github.com>
2023-11-11 14:05:00 -0500

    drive edited

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTest.java
@@ -1,33 +1,36 @@
 package org.firstinspires.ftc.teamcode;
 
-
 import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.arcrobotics.ftclib.gamepad.GamepadKeys;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp
-public class DriveTest extends LinearOpMode {
+public class DriveTest extends LinearOpMode{
+
     private GamepadEx gp1;
     Bot bot;
-    private double driveSpeed =1;
+    private double driveSpeed=1;
+
     @Override
     public void runOpMode() throws InterruptedException {
-        Bot.instance = null;
+
         bot = Bot.getInstance(this);
         gp1 = new GamepadEx(gamepad1);
-        waitForStart();
 
-        while(opModeIsActive() && !isStopRequested()){
-            bot.reverseMotors();
+        waitForStart();
+        bot.reverseMotors();
+
+        while (opModeIsActive() && !isStopRequested()) {
+            telemetry.addData("TeleOp has started","wheeeee");
             drive();
         }
 
     }
 
     private void drive() {
+        gp1.readButtons();
 
         driveSpeed = 1;
 
