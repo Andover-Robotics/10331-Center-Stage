2ece68ed473e0ada6b934489a1952ccbf0e445b0
anikanagle <nikunagle@gmail.com>
2023-11-27 15:04:22 -0500

    Added counter-roller

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Noodles.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Noodles.java
@@ -1,34 +1,46 @@
 package org.firstinspires.ftc.teamcode.subsystems;
+
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 
 public class Noodles {
     //we need a sensor to sense how many pixels we are intaking!
     public final DcMotorEx noodleMotor;
+    public final CRServo counterRoller;
     private final OpMode opMode;
     private boolean isIntake;
 
     public Noodles(OpMode opMode) {
         this.opMode = opMode;
         noodleMotor= opMode.hardwareMap.get(DcMotorEx.class, "noodles motor");
+        counterRoller= opMode.hardwareMap.get(CRServo.class, "counter roller");
+        counterRoller.setDirection(CRServo.Direction.FORWARD);
         noodleMotor.setDirection(DcMotorEx.Direction.REVERSE);
         isIntake = false;
     }
 
     public void intake(){
+        counterRoller.setDirection(CRServo.Direction.FORWARD);
+        noodleMotor.setDirection(DcMotorEx.Direction.REVERSE);
         noodleMotor.setPower(1);
+        counterRoller.setPower(1);
         isIntake=true;
     }
 
     public void intake(double power){
+        counterRoller.setDirection(CRServo.Direction.FORWARD);
+        noodleMotor.setDirection(DcMotorEx.Direction.REVERSE);
         noodleMotor.setPower(power);
+        counterRoller.setPower(1);
         isIntake=true;
     }
 
     public void stop(){
         isIntake= false;
         noodleMotor.setPower(0);
+        counterRoller.setPower(0);
     }
 
 
@@ -37,6 +49,8 @@ public class Noodles {
         noodleMotor.setPower(0.5);
         isIntake=false;
         noodleMotor.setPower(0.5);
+        counterRoller.setDirection(DcMotorEx.Direction.REVERSE);
+        counterRoller.setPower(1);
     }
 
     public boolean getIntakeState(){
