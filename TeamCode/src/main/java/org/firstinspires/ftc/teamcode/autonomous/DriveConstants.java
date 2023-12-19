1b2ead52c0a6ac43e6d485d19ba3b02ab6f4b303
anikanagle <nikunagle@gmail.com>
2023-12-19 17:46:05 -0500

    Tuning constants

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/DriveConstants.java
@@ -44,7 +44,8 @@ public class DriveConstants {
      */
     public static double WHEEL_RADIUS = 1.8898; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (motor) speed
-    public static double TRACK_WIDTH = 10.8; // in
+    //public static double TRACK_WIDTH = 10.8; // in
+    public static double TRACK_WIDTH = 12.5;
 
     /*
      * These are the feedforward parameters used to model the drive motor behavior. If you are using
@@ -52,9 +53,9 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 1.0/(rpmToVelocity(MAX_RPM));
-    public static double kA = 0;
-    public static double kStatic = 0.0;
+    public static double kV = 0.015045745489133502;
+    public static double kA = 0.00152;
+    public static double kStatic = 0.001;
 
     /* 0
      * These values are used to generate the trajectories for you robot. To ensure proper operation,
@@ -94,9 +95,10 @@ public class DriveConstants {
 
      */
 
-    public static double MAX_VEL = 7.233222822378613;
-    public static double MAX_ACCEL = 7.233222822378613;
-    public static double MAX_ANG_VEL = Math.toRadians(38.37343888888889);
+    public static double MAX_VEL = ((MAX_RPM / 60) * GEAR_RATIO * WHEEL_RADIUS * 2 * Math.PI) * 0.85;
+    public static double MAX_ACCEL = ((MAX_RPM / 60) * GEAR_RATIO * WHEEL_RADIUS * 2 * Math.PI) * 0.55;
+   //public static double MAX_ANG_VEL = Math.toRadians(38.37343888888889);
+   public static double MAX_ANG_VEL = Math.toRadians(132.46784704269953);
     public static double MAX_ANG_ACCEL = Math.toRadians(38.37343888888889);
 
     public static RevHubOrientationOnRobot.LogoFacingDirection LOGO_FACING_DIR =
