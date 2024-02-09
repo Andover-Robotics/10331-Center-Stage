f9bba4accd38dcca6c7d2587e704097009b9df61
anikanagle <nikunagle@gmail.com>
2024-02-09 10:30:14 -0500

    Tuning Odometry day 3 - tuned max angle vel - tuned track width - To Do: back and forth (Heading PID)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/DriveConstants.java
@@ -45,7 +45,7 @@ public class DriveConstants {
     public static double WHEEL_RADIUS = 1.8898; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (motor) speed
     //public static double TRACK_WIDTH = 10.8; // in
-    public static double TRACK_WIDTH = 13;
+    public static double TRACK_WIDTH = 14.95;
 
     /*
      * These are the feedforward parameters used to model the drive motor behavior. If you are using
@@ -103,7 +103,7 @@ public class DriveConstants {
     //52.4829190833
     public static double MAX_ACCEL = ((MAX_RPM / 60) * GEAR_RATIO * WHEEL_RADIUS * 2 * Math.PI) * 0.55;
     //33.9595358774
-   public static double MAX_ANG_VEL = Math.toRadians(132.46784704269953);
+   public static double MAX_ANG_VEL = 0.4133333206176758;
    //2.312000083917605
     public static double MAX_ANG_ACCEL = Math.toRadians(38.37343888888889);
 //0.6697428539222843
